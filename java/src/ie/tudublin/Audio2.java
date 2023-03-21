8f270f68dbfb727148d6c02c52ace4d3ee0b0239
fionn seville <fionnseville@gmai.com>
2023-03-21 11:49:24 +0000

    lab6

--- java/src/ie/tudublin/Audio2.java
+++ java/src/ie/tudublin/Audio2.java
@@ -88,4 +88,9 @@ public class Audio2 extends PApplet{
 
         return d + (howFar / range1) * range2;
     }
+
+    public class Pitchspeller{
+        float[] frequencies = {293.66f, 329.63f, 369.99f, 392.00f, 440.00f, 493.88f, 554.37f, 587.33f, 659.25f, 739.99f, 783.99f, 880.00f, 987.77f, 1108.73f, 1174.66f};
+	    String[] spellings = {"D,", "E,", "F,", "G,", "A,", "B,", "C", "D", "E", "F", "G", "A", "B","c", "d", "e", "f", "g", "a", "b", "c'", "d'", "e'", "f'", "g'", "a'", "b'", "c''", "d''"};
+    }
 }
